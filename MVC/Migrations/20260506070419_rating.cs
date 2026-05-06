0c1e3773130c6b316a6d2bdb79e9f64c93b78101
unknown <412ngan@gmail.com>
2026-05-06 14:08:24 +0700

    add filter and new field

---
+++ MVC/Migrations/20260506070419_rating.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class rating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+        }
+    }
+}
